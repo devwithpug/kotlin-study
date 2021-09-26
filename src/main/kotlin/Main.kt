446e59a91a30a1a5a6599f8785c47c93f85fd54d
devwithpug <devwithpug@kakao.com>
2021-09-27 00:41:07 +0900

    210927

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
+import java.util.*
 import kotlin.IllegalStateException
+import kotlin.NoSuchElementException
+import kotlin.random.Random
 
 open class Shape()
 
@@ -19,6 +22,7 @@ data class CustomerDto(
     var addr: String
 )
 
+@OptIn(ExperimentalStdlibApi::class)
 fun main(args: Array<String>) {
     println("Hello World!")
 
@@ -224,6 +228,211 @@ fun main(args: Array<String>) {
         println(err)
     }
 
+    val stringConcat = "before"
+    val newStringConcat = "$stringConcat after"
+    println(newStringConcat)
+
+    val buildStr = buildString {
+        for (i in 1..25 step 2) {
+            append(i)
+        }
+    }
+    println(buildStr)
+
+    val buildLis = buildList {
+        for (i in 1..10) {
+            add(i)
+        }
+    }
+    println(buildLis)
+
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    val invertedOddNumbers = numbers
+        .filter { it % 2 != 0 }
+        .map { -it }
+    println(invertedOddNumbers)
+
+    for (i in 1..100) {
+        print("${Random.nextBoolean()} ")
+    }
+    println()
+
+    val name = (if (Random.nextBoolean()) "" else "TRUE").ifBlank { "OH" }
+    println(name)
+
+    val input = "##plcae##holder##"
+    println(input.removeSurrounding("##"))
+
+    val regex = Regex("""\w*\d+\w*""")
+    val input2 = "login: Pokemon5, password: 1q2w3e4r"
+    val replaceResult = regex.replace(input2, replacement = "xxx")
+    println(replaceResult)
+
+    println("La.La.Means.I.Love.You".split("."))
+
+
+    val input3 = "What is Love? Baby Don't hurt me? No more."
+    println(input3.substringAfter("?"))
+    println(input3.substringAfterLast("?"))
+
+    println(
+        """
+            자동으로 줄바꿈을
+            해준다고 합니다
+                인덴트도 되는건가요?
+                    와우!!!
+            오집니다잉
+        """.trimIndent()
+    )
+
+    println(
+        """
+            #in-line
+            # with-space
+            #   with-indent
+        """.trimMargin("#")
+    )
+
+    val trimIndent = """
+        TEST
+            TEST
+    """.trimIndent()
+    println()
+
+    val myLis = listOf("A", "B", "C")
+
+    println(myLis.joinToString { "[$it]" })
+    println(myLis.joinToString(prefix = "[", postfix = "]"))
+
+//    throwExample(2)
+
+    val d1 = MyDate(2021, 10, 26)
+    val d2 = MyDate(2021, 11, 5)
+
+    for (d in d1..d2) {
+        println("${d.year} | ${d.month} | ${d.dayOfMonth}")
+    }
+
+    println(listOf("1", "1", "2", "3", "5").toSet())
+
+    val listOf =
+        listOf(listOf("1", "1", "2", "3", "5"), listOf("A", "A", "B", "C", "E"), listOf("a", "a", "b", "c", "e"))
+
+    val flatMap = listOf.flatMap { it }
+    println(flatMap)
+
+    val members = listOf(
+        Member("memberA", Address("오산", "코리아", 11), 1, listOf("ABC", "CBA")),
+        Member("memberB", Address("광주", "코리아", 22), 2, listOf("ABC", "CBA")),
+        Member("memberC", Address("수원", "한국", 33), 3, listOf("ABC", "CBA")),
+        Member("memberD", Address("서울", "코리아", 44), 4, listOf("ABC", "CBA")),
+        Member("memberE", Address("제주", "한국", 55), 5, listOf("ABC", "CBA")),
+        Member("memberF", Address("세종", "대한민국", 66), 6, listOf("ABC", "CBA")),
+    )
+
+    println(members.associateBy { it.name })
+    println(members.associateWith { it.addr })
+    println(members.associate { it.name to it.addr })
+
+    println(members.groupBy { it.addr.country })
+
+    val (big, small) = members.partition { it.addr.code > 30 }
+    println("""
+        big     : $big
+        small   : $small
+    """.trimIndent())
+
+    val joinToString = members
+        .flatMap { it.types }
+        .joinToString()
+    println(joinToString)
+
+    println(members
+        .fold(1) { t, e -> t + e.num })
+
+}
+
+data class Member(
+    val name: String,
+    val addr: Address,
+    val num: Int,
+    val types: List<String>
+)
+
+data class Address(
+    val city: String,
+    val country: String,
+    val code: Int
+)
+
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    override fun compareTo(other: MyDate): Int {
+        if (year != other.year) return year - other.year
+        if (month != other.month) return month - other.month
+        return dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate =
+    addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
+
+fun MyDate.addTimeIntervals(timeInterval: TimeInterval, amount: Int): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year + if (timeInterval == TimeInterval.YEAR) amount else 0, month, dayOfMonth)
+    var timeInMillis = c.timeInMillis
+    val millisecondsInADay = 24 * 64 * 64 * 1000L
+    timeInMillis += amount * when (timeInterval) {
+        TimeInterval.DAY -> millisecondsInADay
+        TimeInterval.WEEK -> 7 * millisecondsInADay
+        TimeInterval.YEAR -> 0L
+    }
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+fun MyDate.followingDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth)
+    val m = 24 * 60 * 60 * 1000L
+    val timeInM = c.timeInMillis + m
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInM
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current = start
+
+            override fun hasNext(): Boolean = current <= end
+
+            override fun next(): MyDate {
+                if (!hasNext()) throw NoSuchElementException()
+                val result = current
+                current = current.followingDate()
+                return result
+            }
+        }
+    }
+}
+
+fun containsEven(collection: Collection<Int>): Boolean =
+    collection.any { it % 2 == 0 }
+
+fun throwExample(num: Int?): Nothing {
+    throw IllegalArgumentException("exception Example $num")
+}
+
+fun checkAge(age: Int?) {
+    if (age == null || age !in 0..150) throwExample(age)
+    println("Congrats! Next year you'll be ${age + 1}.")
 }
 
 class TestClass {
