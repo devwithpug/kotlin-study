e2c1650f98cd9f22d2940c097419104f4602e57f
devwihtpug <devwithpug@kakao.com>
2022-02-26 20:43:38 +0900

    ch5: collection function api

---
+++ kotlin-in-action/src/main/kotlin/ch4/CollectionFunctionalAPI.kt
@@ -0,0 +1,35 @@
+package ch4
+
+fun main() {
+    val people = listOf(
+        Person("John", 5),
+        Person("Kim", 10),
+        Person("Park", 15),
+        Person("Lee", 20),
+        Person("James", 25)
+    )
+
+    val result = people
+        .filter { it.age >= 10 }
+        .map { it.name }
+        .groupBy { it.length }
+
+    println(result)
+
+    flatten()
+    withSequence(people)
+}
+
+fun flatten() {
+    val strings = listOf("abc", "def")
+    println(strings.flatMap { it.toList() })
+}
+
+fun withSequence(people: List<Person>) {
+    val result = people.asSequence()
+        .filter { it.age >= 10 }
+        .map { it.name }
+        .groupBy { it.length }
+
+    println(result)
+}
