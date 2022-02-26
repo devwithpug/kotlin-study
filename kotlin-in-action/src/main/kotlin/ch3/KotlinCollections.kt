ce1a87b45caa63dca0c440c9c22198764e94f688
devwihtpug <devwithpug@kakao.com>
2022-02-26 16:16:43 +0900

    ch3.4

--- kotlin-in-action/src/main/kotlin/ch3/KotlinCollections.kt
+++ kotlin-in-action/src/main/kotlin/ch3/KotlinCollections.kt
@@ -1,5 +1,6 @@
 package ch3
 
+import java.time.LocalDate
 
 // 코틀린은 자신만의 컬렉션 기능을 제공하지 않는다. (자바 컬렉션을 사용)
 fun main() {
@@ -15,6 +16,10 @@ fun main() {
 
     val joinToString = list.joinToString() // default parameter values
     println(joinToString)
+
+    for ((index, element) in list.withIndex()) {
+        println("$index : $element")
+    }
 }
 
 // 최상위 변수
@@ -25,3 +30,20 @@ const val SOME_CONST_VALUE = 100
 fun topFunction() {
     topVariable += SOME_CONST_VALUE
 }
+
+fun sumWithVararg(vararg elements: Int): Int = elements.sum()
+
+fun spreadOperator(args: Array<Int>): List<Int> {
+    return listOf(*args)
+}
+
+val map = mapOf(1 to "one", 2 to "two", 3 to "three")
+val map2 = mapOf(Pair(1, "one"), Pair(2, "two"), Pair(3, "three"))
+
+val pair = 10 to "ten"
+
+infix fun LocalDate.to(toDate: LocalDate) = this.rangeTo(toDate)
+
+val startDate: LocalDate = LocalDate.of(2022, 2, 20)
+
+val range: ClosedRange<LocalDate> = startDate to LocalDate.now()
