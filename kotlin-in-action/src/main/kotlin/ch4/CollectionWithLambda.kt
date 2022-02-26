da8c3e7034cd12d34f0abb39c84d74c3bddfc26b
devwihtpug <devwithpug@kakao.com>
2022-02-26 18:06:14 +0900

    ch5: Collections with lambda refactoring

--- kotlin-in-action/src/main/kotlin/ch4/CollectionWithLambda.kt
+++ kotlin-in-action/src/main/kotlin/ch4/CollectionWithLambda.kt
@@ -27,4 +27,8 @@ fun main() {
 
     people.maxByOrNull(Person::age)
     // Optional<Person> max = people.stream().max(Person::getAge);
+
+    people.maxByOrNull { p: Person -> p.age } // 함수의 맨 마지막 인자가 람다 식이므로 괄호 밖으로 빼낼 수 있다.
+    people.maxByOrNull { p -> p.age } // 컴파일러가 문맥으로부터 유추할 수 있는 인자 타입을 생략할 수 있다.
+    people.maxByOrNull { it.age } // 인자가 단 하나 뿐인 경우 인자에 이름을 붙이지 않아도 된다.
 }
