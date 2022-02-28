aa28cf31fd709ad5e56fcff3e2f034e1364f28f2
devwihtpug <devwithpug@kakao.com>
2022-02-28 23:22:19 +0900

    ch4: abstract modifier

--- kotlin-in-action/src/main/kotlin/ch4/KotlinModifier.kt
+++ kotlin-in-action/src/main/kotlin/ch4/KotlinModifier.kt
@@ -5,3 +5,9 @@ open class RichButton : Clickable {
     open fun animate() {} // open method 오버라이드 가능
     override fun click() {} // override method 기본적으로 open 이다.
 }
+
+abstract class Animated { // abstract class
+    abstract fun animate() // abstract method
+    open fun stopAnimating() {}
+    fun animateTwice() {}
+}
