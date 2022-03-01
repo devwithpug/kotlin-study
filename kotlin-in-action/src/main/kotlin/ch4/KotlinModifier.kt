3ba45b8fd846702683637a173816bb130f7e49c6
devwihtpug <devwithpug@kakao.com>
2022-03-01 20:35:26 +0900

    ch4: visibility modifier

--- kotlin-in-action/src/main/kotlin/ch4/KotlinModifier.kt
+++ kotlin-in-action/src/main/kotlin/ch4/KotlinModifier.kt
@@ -11,3 +11,18 @@ abstract class Animated { // abstract class
     open fun stopAnimating() {}
     fun animateTwice() {}
 }
+
+// 가시성 변경자
+// public, internal, protected, private
+
+internal open class TalkativeButton : Clickable {
+    override fun click() {}
+    private fun yell() {}
+    protected fun whisper() {}
+}
+
+// fun TalkativeButton.giveSpeech() { // 컴파일에러 public 멤버가 자신의 internal 수신타입을 호출함. 자신의 가시성과 같거나 더 높아야만 한다.
+//     click() // open, public (가능)
+//     yell() // 컴파일에러 private 멤버 호출 불가능
+//     whisper() // 컴파일에러 protected 멤버 호출 불가능
+// }
