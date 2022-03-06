f616344672a71c0bb9d51c517ede9245cff839af
devwithpug <devwithpug@kakao.com>
2022-03-06 22:34:23 +0900

    ch8: kotlin non-local return

---
+++ kotlin-in-action/src/main/kotlin/ch8/NonLocalReturn.kt
@@ -0,0 +1,33 @@
+package ch8
+
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+val people = listOf(Person("Alice", 29), Person("Bob", 31))
+
+fun lookForAlice(people: List<Person>) {
+    for (person in people) {
+        if (person.name == "Alice") {
+            println("Found!")
+            return
+        }
+    }
+    println("Alice is not found")
+}
+
+fun lookForEachAlice(people: List<Person>) {
+    people.forEach {
+        if (it.name == "Alice") {
+            println("Found!")
+            return
+        }
+    }
+    println("Alice is not found")
+}
+
+fun main() {
+    lookForAlice(people) // Found!
+    lookForEachAlice(people) // Found!
+}
