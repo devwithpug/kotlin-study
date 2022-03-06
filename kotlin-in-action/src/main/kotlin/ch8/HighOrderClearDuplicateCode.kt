734e907a94b09262104b883d4ac52a162358158e
devwithpug <devwithpug@kakao.com>
2022-03-06 20:34:01 +0900

    ch8: high order clear duplicate code

---
+++ kotlin-in-action/src/main/kotlin/ch8/HighOrderClearDuplicateCode.kt
@@ -0,0 +1,43 @@
+package ch8
+
+data class SiteVisit(
+    val path: String,
+    val duration: Double,
+    val os: OS
+)
+
+enum class OS { WINDOWS, LINUX, MAC, IOS, ANDROID }
+
+val log = listOf(
+    SiteVisit("/", 34.0, OS.WINDOWS),
+    SiteVisit("/", 22.0, OS.MAC),
+    SiteVisit("/login", 12.0, OS.WINDOWS),
+    SiteVisit("/signup", 8.0, OS.IOS),
+    SiteVisit("/", 16.3, OS.ANDROID),
+)
+
+fun List<SiteVisit>.averageDurationFor(os: OS) =
+    filter { it.os == os }.map { it.duration }.average()
+
+fun List<SiteVisit>.averageDurationFor(predicate: (SiteVisit) -> Boolean) =
+    filter(predicate).map { it.duration }.average()
+
+fun main() {
+    val averageWindowsDuration = log
+        .filter { it.os == OS.WINDOWS }
+        .map { it.duration }
+        .average()
+
+    val averageMacDuration = log
+        .filter { it.os == OS.MAC }
+        .map { it.duration }
+        .average()
+
+    println(averageWindowsDuration) // 23.0
+    println(averageMacDuration) // 22.0
+
+    println(log.averageDurationFor(OS.WINDOWS))
+    println(log.averageDurationFor(OS.MAC))
+
+    println(log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" }) // 8.0
+}
