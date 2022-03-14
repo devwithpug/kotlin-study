6f2a481a13cb464408f769a101f780084136e64d
devwihtpug <devwithpug@kakao.com>
2022-03-14 21:30:29 +0900

    add dependency

--- kotlin-coroutines-study/build.gradle.kts
+++ kotlin-coroutines-study/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
