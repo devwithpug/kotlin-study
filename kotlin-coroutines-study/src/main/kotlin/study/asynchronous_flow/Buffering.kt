a20dce5d8515f853018d7a777e9a4ae3e83324fc
devwithpug <devwithpug@kakao.com>
2022-03-19 16:59:17 +0900

    buffering

--- kotlin-coroutines-study/src/main/kotlin/study/asynchronous_flow/Buffering.kt
+++ kotlin-coroutines-study/src/main/kotlin/study/asynchronous_flow/Buffering.kt
@@ -1,27 +1,59 @@
 package study.asynchronous_flow
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import study.log
+import kotlin.coroutines.coroutineContext
 import kotlin.system.measureTimeMillis
 
-fun simpleBuffering() = flow {
-    for (i in 1..3) {
-        log("emitting... $i")
+fun simpleDelayFlow() = flow {
+    for (i in 1..10) {
+        log("$coroutineContext emitting... $i")
         delay(100)
         emit(i)
     }
 }
 
-fun main() = runBlocking {
+suspend fun doBuffer() = coroutineScope {
     val time = measureTimeMillis {
-        simpleBuffering().buffer().collect {
-            log("buffer $it")
+        simpleDelayFlow().buffer().collect {
+            log("$coroutineContext buffer $it")
+            delay(500)
+            println(it)
+        }
+    }
+    println("Collected in $time ms")
+}
+
+suspend fun doConflate() = coroutineScope {
+    val time = measureTimeMillis {
+        simpleDelayFlow().conflate().collect {
+            log("$coroutineContext conflate $it")
+            delay(500)
+            println(it)
+        }
+    }
+    println("Collected in $time ms")
+}
+
+suspend fun doCollectLatest() = coroutineScope {
+    val time = measureTimeMillis {
+        simpleDelayFlow().collectLatest {
+            log("$coroutineContext collecting $it")
             delay(300)
             println(it)
         }
     }
     println("Collected in $time ms")
+}
+
+fun main() = runBlocking {
+    // doBuffer()
+    // doConflate()
+    doCollectLatest()
 }
